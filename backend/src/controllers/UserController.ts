3ebde999959f4bbcdeba133c35b492d51fdab7c5
malurmaia <marialuisaribeiromaia@gmail.com>
2022-12-21 21:38:55 -0300

    implementa register

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import User from '../models/User';
 import { Op } from 'sequelize';
 const bcrypt = require("bcryptjs")
+import IUser from '../interfaces/iuser';
 
 class UserController {
     user;
@@ -27,7 +28,7 @@ class UserController {
                 return {
                     status: 200,
                     data: {
-                        msg: 'Login Successful',
+                        msg: 'Logado com sucesso',
                         user: {
                             id,
                             username,
@@ -42,18 +43,49 @@ class UserController {
         }
         return {
             status: 401,
-            data: 'Invalid Data'
+            data: 'dado inválido'
         }
     }
-    async  register(data: Response){
-        return{
-            status: 200,
-            data: {}
+    async register(data: IUser) {
+        let { username, email, name, password } = data;
+        let user: any = await User.findOne({
+            where: {
+                username: username,
+            }
+
+        })
+        if (user) {
+            return {
+                status: 409,
+                data: { mag: "Username em uso" }
+            }
+        }
+        user = await User.findOne({
+            where: {
+                email: email,
+            }
+        })
+
+        if (user) {
+            return {
+                status: 409,
+                data: { mag: "Este email já foi cadastrado." }
+            }
         }
 
-
+        user = await User.create({
+            username,
+            email,
+            name,
+            password: bcrypt.hashSync(password, 10)
+        });
+        return {
+            status: 201,
+            data: {
+                msg: "Usuário criado com sucesso."
+            }
+        }
     }
-
 }
 
 export default UserController;
